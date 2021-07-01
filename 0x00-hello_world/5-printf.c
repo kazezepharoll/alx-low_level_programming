6324bd1a6d26f8d2ecef56a966a5d16aee8b6a53
kazezepharoll <kazezepharoll47@gmail.com>
2021-07-01 23:35:35 +0000

    a printf file committed

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
 #include<string.h>
 
+/**
+ *This is a Printf main program which prints out some string
+ */
 int main(void)
 {
-printf("with proper grammer, but the outcome is a piece of art,\n");
-      
+printf("with proper grammar, but the outcome is a piece of art,\n");      
 return (0);
 
 }
