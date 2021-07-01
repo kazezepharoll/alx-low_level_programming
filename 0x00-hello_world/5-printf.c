a0812f898b292d4ad86fddb93ffa75c9997d13eb
kazezepharoll <kazezepharoll47@gmail.com>
2021-07-01 23:27:42 +0000

    a printf file committed

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 #include<string.h>
 
-int main()
+int main(void)
 {
-  printf("with proper grammer, but the outcome is a piece of art,\n");
-  return 0;
+printf("with proper grammer, but the outcome is a piece of art,\n");
+      
+return (0);
 
 }
