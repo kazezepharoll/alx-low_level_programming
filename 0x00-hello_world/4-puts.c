98d2f61495c4b7872b4b58acf544f0752058bf0c
kazezepharoll <kazezepharoll47@gmail.com>
2021-07-03 15:09:48 +0000

    committing a puts file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 
 /**
- *
- *main - prints string
- *
- *Return : returns a 0
- *
- */
+*main - use printf typeof
+*Description: output with printf
+*Return: 0
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
