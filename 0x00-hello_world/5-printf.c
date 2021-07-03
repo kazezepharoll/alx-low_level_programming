112fb299678430ab9f87508779dd954fc19a441d
kazezepharoll <kazezepharoll47@gmail.com>
2021-07-03 14:47:59 +0000

    edit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,12 @@
 
 /**
 * main - prints string
-*   
+*
 * Return: 0 upon successful compilation
-* 
+*
 */
 int main(void)
 {
-   printf("with proper grammar, but the outcome is a piece of art,\n");
-   return (0);
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+  return (0);
 }
