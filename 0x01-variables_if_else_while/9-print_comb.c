fc3d99ca2b384ecdb1e7758118b93dac0dea060e
kazezepharoll <kazezepharoll47@gmail.com>
2021-07-04 04:07:36 +0000

    Printing combination of numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,18 +11,16 @@
 */
 int main(void)
 {
-int c:
+int c;
 
-c = '0';
-while (c <= '9')
+for (c = '0'; c <= '9';c++)
 {
 putchar(c);
-if (c < '9')
+if ( c < '9')
 {
 putchar(',');
 putchar(' ');
 }
-c++;
 }
 putchar('\n');
 return (0);
