5b90e1c47adf3b80f1d00cbf91cb40269188663f
kazezepharoll <kazezepharoll47@gmail.com>
2021-07-04 01:59:41 +0000

    To uppercase conversion

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main-entry point
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+char chr;
+
+for (chr = 'a' ; chr <= 'z' ; chr++)
+putchar (chr);
+putchar(toupper(chr));
+putchar ('\n');
+return (0);
+}
