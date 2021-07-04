e4112427fa021d9259dffe7dc8f714f3e9691c28
kazezepharoll <kazezepharoll47@gmail.com>
2021-07-04 03:12:16 +0000

    commiting the 5th task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+* main-entry point
+* Description: print numbers from 0 to 9
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+int i;
+for(i=0; i<10; i++)
+printf("%d", i);
+printf("\n");
+return (0);
+}
